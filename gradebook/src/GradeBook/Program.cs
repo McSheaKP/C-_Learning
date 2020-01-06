af104cdbe675bb180a5329d391f3c68741d8be7a
Kevin McShea <kevinmcshea@gmail.com>
2020-01-05 18:02:55 -0800

    Initial commit on C# Fundamentals

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+            int numGrades = grades.Count;
+            double total = 0;
+            foreach( var grade in grades)
+            {
+                total = grade + total;
+            }
+            total /= numGrades;
+
+            Console.Write($"My average grade is equal to {total}");
+        }
+    }
+}
