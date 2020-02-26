f7ff391b381be53d065aa5c662d76ebad08c7f22
mcsheakp <kevinmcshea@gmail.com>
2020-02-25 22:32:39 -0800

    finsihed module on reference and value types

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -3,8 +3,81 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
+    
     public class TypeTests
     {
+        [Fact]
+        public void Test1()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int x)
+        {
+            x = 42;
+        }
+
+        public int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetNameByRef(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        public void GetBookSetNameByRef(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+
+
+        public void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Scott";
+            name = MakeUpperCase(name);
+
+            Assert.Equal("SCOTT", name);
+        }
+
+        private string MakeUpperCase(string name)
+        {
+            return name.ToUpper();
+        }
+
         [Fact]
         public void CanChangeNameBooks()
         {
